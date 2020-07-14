6ad6d478a7755179bbdd985d1e898859f31ec7b4
habib <habib.movludov@aurorabilisim.com>
2020-07-14 17:23:10 +0300

    http rest api

---
+++ src/app/employee-create/employee-create.component.ts
@@ -0,0 +1,28 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {RestApiService} from '../../../shared/rest-api.service';
+import {Route, Router} from '@angular/router';
+
+@Component({
+  selector: 'app-employee-create',
+  templateUrl: './employee-create.component.html',
+  styleUrls: ['./employee-create.component.css']
+})
+export class EmployeeCreateComponent implements OnInit {
+  @Input() employeeDetails = {name: '', email: '', phone: 0};
+
+  constructor(
+    public  restApi: RestApiService,
+    public router: Router
+  ) {
+  }
+
+  ngOnInit() {
+  }
+
+  addEmployee(dataEmployee) {
+    this.restApi.createEmployee(this.employeeDetails).subscribe((data: {}) => {
+      this.router.navigate(['/employees-list']);
+    });
+  }
+
+}
